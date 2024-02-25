37dde059d307691f3a311c35775ca6a0fd2358dc
Hanjun Kwon <kwondns@gmail.com>
2024-02-26 06:16:24 +0900

    🚀 Feat: FetchLib
    - Post, Get
    - AccessToken 첨부 시 인증 추가
    - RefreshToken 첨부를 위해 credentials 설정

---
+++ src/lib/fetch.ts
@@ -0,0 +1,46 @@
+const resultHandler = async (result: Response) => {
+  const res = await result.json();
+  switch (result.status) {
+    case 200: {
+      return res;
+    }
+    case 201: {
+      return res;
+    }
+    case 204: {
+      return res;
+    }
+    case 400: {
+      throw new Error(res.message);
+    }
+    case 401: {
+      throw new Error(res.message);
+    }
+    default: {
+      throw new Error(res.message);
+    }
+  }
+};
+
+export const PostFetch = async <B, R>(url: string, body: B, accessToken?: string): Promise<R> => {
+  const headers = { 'Content-type': 'application/json' };
+  if (accessToken) Object.assign(headers, { Authorization: `Bearer ${accessToken}` });
+  const result = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/${url}`, {
+    method: 'POST',
+    credentials: 'include',
+    headers,
+    body: JSON.stringify(body),
+  });
+  return resultHandler(result);
+};
+
+export const GetFetch = async <T>(url: string, accessToken?: string): Promise<T> => {
+  const headers = { 'Content-type': 'application/json' };
+  if (accessToken) Object.assign(headers, { Authorization: `Bearer ${accessToken}` });
+  const result = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/${url}`, {
+    method: 'GET',
+    credentials: 'include',
+    headers,
+  });
+  return resultHandler(result);
+};
