b743019b3f3d7916c3f37b1b740f1a70a3592c68
Hanjun Kwon <kwondns@gmail.com>
2024-02-26 06:24:18 +0900

    🎨 Style: export default 설정
    - 단일 export 시 default 설정

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,4 +1,4 @@
-export function isTokenExpired(token: string) {
+export default function isTokenExpired(token: string) {
   try {
     const payload = JSON.parse(atob(token.split('.')[1]));
     const now = Math.floor(Date.now() / 1000);
