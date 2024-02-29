7961c7df51069e8b0bb0858c594318a338f905c2
Hanjun Kwon <kwondns@gmail.com>
2024-03-01 02:18:28 +0900

    🚀 Feat: Put Fetch 추가
    - 업데이트 전용 Put 메소드 추가

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -44,3 +44,15 @@ export const GetFetch = async <T>(url: string, accessToken?: string): Promise<T>
   });
   return resultHandler(result);
 };
+
+export const PutFetch = async <B, R>(url: string, body: B, accessToken?: string): Promise<R> => {
+  const headers = { 'Content-type': 'application/json' };
+  if (accessToken) Object.assign(headers, { Authorization: `Bearer ${accessToken}` });
+  const result = await fetch(`${import.meta.env.VITE_API_SERVER_URL}/${url}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers,
+    body: JSON.stringify(body),
+  });
+  return resultHandler(result);
+};
