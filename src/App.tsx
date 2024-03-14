0e383f1f99eede1c0dc0483e0d3f56d2bc4c08af
kwondns <155633601+kwondns@users.noreply.github.com>
2024-03-14 23:04:44 +0900

    Revert "Merge Timeline To Dev"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { RecoilRoot } from 'recoil';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-import Router from '@/router';
-
 function App() {
   return (
-    <RecoilRoot>
-      <ToastContainer pauseOnFocusLoss={false} />
-      <Router />
-    </RecoilRoot>
+    <div>hello world!
+    </div>
   );
 }
 
--- src/types/TimeLine/past.type.ts
+++
@@ -1,9 +0,0 @@
-export type PastCountType = { id: string; date: string; count: number; titles: string[] | null; titles_count: number };
-
-export type PastType = {
-  id: string;
-  title: string;
-  content: string;
-  startTime: string;
-  endTime: string;
-};
